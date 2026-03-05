ad7ebf3742f6446ab588f154a1218d1e5106cbdb
Pliffdax <stepanov-oleksandr@ukr.net>
2026-03-05 10:29:05 +0200

    fix(pages): wrong basse-url specified

--- base-path.js
+++ base-path.js
@@ -1,3 +1,5 @@
 export const BASE_PATH =
   process.env.BASE_PATH ??
-  (process.env.NODE_ENV === "production" ? "/Labs-Markdown-Template" : "");
+  (process.env.NODE_ENV === "production"
+    ? "/IO-35_appRECORD-StepanovOleksandr-FIOT-2026"
+    : "");
